1cd630b6a096d6490ef85603cf63eb86942c0b60
Jeff Weiss <jeff.weiss@puppetlabs.com>
2012-05-21 22:33:43 -0700

    add first pass at puppet module auto-publish on tag

---
+++ services/puppet_module.rb
@@ -0,0 +1,22 @@
+require 'pp'
+class Service::PuppetModule < Service
+  string :username
+  string :api_token
+  
+  def receive_push
+    if tag? and created?
+      pp "new tag (#{tag_name}) at #{tag_download_url}"
+    else
+      pp "not a tag:\n#{payload}"
+    end
+  end
+  
+  def tag_name
+    ref =~ /^refs\/tags\.(.*)/
+    $1
+  end
+  
+  def tag_download_url(type=:tarball)
+    "#{repo_url}/#{type}/#{tag_name}"
+  end
+end

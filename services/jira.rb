5d7e2270700f222b73ac389b2a9d3a893d500ed1
Jeff Weiss <jeff.weiss@puppetlabs.com>
2012-09-20 00:30:39 -0700

    first pass of pull request support for jira

--- services/jira.rb
+++ services/jira.rb
@@ -1,7 +1,28 @@
 class Service::Jira < Service
-  string   :server_url, :api_version, :username
+  string   :server_url, :api_version, :username, :pull_transition
   password :password
-  white_list :api_version, :server_url, :username
+  white_list :api_version, :server_url, :username, :pull_transition
+  
+  default_events :push, :pull_request
+
+  def receive_pull_request
+    pull.title.match /\(#(.*?)\)/
+    return unless $1
+    issue_id = $1
+    comment_body = "#{pull.title}\n#{pull.body}"
+    changeset = { :comment => { :body => comment_body } }
+    changeset.merge! :transition => data['pull_transition']
+    
+    begin
+      http.ssl[:verify] = false
+
+      http.basic_auth data['username'], data['password']
+      http.headers['Content-type'] = 'application/json'
+      res = http_post '%s/rest/api/%s/issue/%s/transitions' % [data['server_url'], data['api_version'], issue_id], changeset.to_json
+    rescue URI::InvalidURIError
+      raise_config_error "Invalid server_hostname: #{data['server_url']}"
+    end
+  end
 
   def receive_push
     payload['commits'].each do |commit|

aa1b9b7a9cadfa226a61a0bcd67b997149acdc8e
Jeff Weiss <jeff.weiss@puppetlabs.com>
2012-09-20 00:38:03 -0700

    jira service refactor of api communication

--- services/jira.rb
+++ services/jira.rb
@@ -13,7 +13,12 @@ class Service::Jira < Service
     changeset = { :comment => { :body => comment_body } }
     changeset.merge! :transition => data['pull_transition']
     
+    send_issue_changeset issue_id, changeset
+  end
+  
+  def send_issue_changeset(issue_id, changeset)
     begin
+      # :(
       http.ssl[:verify] = false
 
       http.basic_auth data['username'], data['password']
@@ -54,17 +59,7 @@ class Service::Jira < Service
       # Don't need to continue if we don't have a transition to perform
       next unless changeset.has_key?(:transition)
 
-      begin
-        # :(
-        http.ssl[:verify] = false
-
-        http.basic_auth data['username'], data['password']
-        http.headers['Content-Type'] = 'application/json'
-        res = http_post '%s/rest/api/%s/issue/%s/transitions' % [data['server_url'], data['api_version'], issue_id],
-          changeset.to_json
-      rescue URI::InvalidURIError
-        raise_config_error "Invalid server_hostname: #{data['server_url']}"
-      end
+      send_issue_changeset issue_id, changeset
     end
   end
 end
